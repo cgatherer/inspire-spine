415bc626bb043b12b53f7a74e269255fa7d5e17f
Christopher Gatherer <chris.gatherer@gmail.com>
2018-01-29 11:51:43 -0700

    Navigation

--- assets/scss/header.php
+++ assets/scss/header.php
@@ -220,6 +220,7 @@
 								<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
 									<img src="<?php echo esc_attr( $logo ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" id="logo" data-height-percentage="<?php echo esc_attr( et_get_option( 'logo_height', '54' ) ); ?>" />
 								</a>
+								<h1>Testing</h1>
 							</div>
 
 							<div id="et-top-navigation" data-height="<?php echo esc_attr( et_get_option( 'menu_height', '66' ) ); ?>" data-fixed-height="<?php echo esc_attr( et_get_option( 'minimized_menu_height', '40' ) ); ?>">
