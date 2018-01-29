0b2183d5acbe9a078b33eb4d6f1b9be483f8f2ad
Christopher Gatherer <chris.gatherer@gmail.com>
2018-01-29 12:57:39 -0700

    Navigation

--- header.php
+++ header.php
@@ -216,12 +216,13 @@
 
 				<div class="span12 group nopad">
 					<div class="span8">
-						<div class="logo_container">
+						<h1>Logo</h1>
+						<!-- <div class="logo_container">
 							<span class="logo_helper"></span>
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
 								<img src="<?php echo esc_attr( $logo ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" id="logo" data-height-percentage="<?php echo esc_attr( et_get_option( 'logo_height', '54' ) ); ?>" />
 							</a>
-						</div>
+						</div> -->
 
 						<!-- <div id="et-top-navigation" data-height="<?php echo esc_attr( et_get_option( 'menu_height', '66' ) ); ?>" data-fixed-height="<?php echo esc_attr( et_get_option( 'minimized_menu_height', '40' ) ); ?>">
 									<?php if ( ! $et_slide_header || is_customize_preview() ) : ?>
