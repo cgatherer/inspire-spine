acc9bcc8cb5e9bb3ae21ece2c01bb4f37b87ca55
Christopher <cgatherer@princetonpartners.com>
2018-01-31 20:46:27 -0500

    Update header.php

--- header.php
+++ header.php
@@ -215,7 +215,7 @@
 				?>
 
 				<div class="span12 group nopad">
-					<div class="span8">
+					<div class="span9">
 						<div class="logo_container">
 							<span class="logo_helper"></span>
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
@@ -273,8 +273,8 @@
 						</div>
 					</div>
 
-					<div class="span4">
-						<?php dynamic_sidebar( 'right-cta' ); ?>
+					<div class="span3">
+						<div class="right"><?php dynamic_sidebar( 'right-cta' ); ?></div>
 					</div>
 				</div>
 		</header> <!-- #main-header -->
