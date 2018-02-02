5bf4fcbab2c2e6ee203da8b4d021e115382b6588
Christopher Gatherer <chris.gatherer@gmail.com>
2018-02-02 16:50:48 -0500

    scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,6 +4,6 @@
 jQuery(document).ready(function($){
 	$('.BG').click(function(e) {
         e.preventDefault();
-        $('.form-area').toggle(500);
+        $('.form-area').slideToggle(500);
     });
 });
