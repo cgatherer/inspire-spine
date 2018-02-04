c02e010f471b9ee85c14de02cdd6d114f517015c
Christopher Gatherer <chris.gatherer@gmail.com>
2018-02-04 10:50:14 -0500

    footer.php

--- footer.php
+++ footer.php
@@ -13,55 +13,35 @@
 
 			<div id="footer-bottom">
 				<div class="container clearfix">
-						<?php
-							if ( false !== et_get_option( 'show_footer_social_icons', true ) ) {
-								get_template_part( 'includes/social_icons', 'footer' );
-							}
-
-							echo et_get_footer_credits();
-						?>
+					<div class="span12 nopad group">
+						<div class="span4">
+							<?php
+								wp_nav_menu( array(
+									'theme_location' => 'footer-menu',
+									'depth'          => '1',
+									'menu_class'     => 'bottom-nav',
+									'container'      => '',
+									'fallback_cb'    => '',
+								) );
+							?>
+						</div>
+						<div class="span4">
+							<?php
+								if ( false !== et_get_option( 'show_footer_social_icons', true ) ) {
+									get_template_part( 'includes/social_icons', 'footer' );
+								}
+							?>
+						</div>
+						<div class="span4">
+							<?php echo et_get_footer_credits(); ?>
+						</div>
+					</div>
 				</div>	<!-- .container -->
 			</div>
-			<?php //get_sidebar( 'footer' ); ?>
+		</footer> <!-- #main-footer -->
+	</div> <!-- #et-main-area -->
+</div> <!-- #page-container -->
 
-
-			<?php
-				//if ( has_nav_menu( 'footer-menu' ) ) : ?>
-
-				<!-- <div id="et-footer-nav">
-					<div class="container"> -->
-						<?php
-							// wp_nav_menu( array(
-							// 	'theme_location' => 'footer-menu',
-							// 	'depth'          => '1',
-							// 	'menu_class'     => 'bottom-nav',
-							// 	'container'      => '',
-							// 	'fallback_cb'    => '',
-							// ) );
-						?>
-					<!-- </div>
-				</div> --> <!-- #et-footer-nav -->
-
-			<?php //endif; ?>
-
-				<!-- <div id="footer-bottom">
-					<div class="container clearfix"> -->
-						<?php
-							// if ( false !== et_get_option( 'show_footer_social_icons', true ) ) {
-							// 	get_template_part( 'includes/social_icons', 'footer' );
-							// }
-
-							// echo et_get_footer_credits();
-						?>
-					<!-- </div> -->	<!-- .container -->
-				<!-- </div> -->
-			</footer> <!-- #main-footer -->
-		</div> <!-- #et-main-area -->
-
-		<?php //endif; // ! is_page_template( 'page-template-blank.php' ) ?>
-
-	</div> <!-- #page-container -->
-
-	<?php wp_footer(); ?>
+<?php wp_footer(); ?>
 </body>
 </html>
