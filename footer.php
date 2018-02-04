6501dceb199b3efa9bdb6635f6a7c0896bfb89e5
Christopher Gatherer <chris.gatherer@gmail.com>
2018-02-04 10:39:17 -0500

    button text

--- footer.php
+++ footer.php
@@ -1,49 +1,64 @@
-<?php if ( 'on' == et_get_option( 'divi_back_to_top', 'false' ) ) : ?>
+		<?php if ( 'on' == et_get_option( 'divi_back_to_top', 'false' ) ) : ?>
 
-	<span class="et_pb_scroll_top et-pb-icon"></span>
+			<span class="et_pb_scroll_top et-pb-icon"></span>
 
-<?php endif;
+		<?php endif; ?>
 
-if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
+		<footer id="main-footer">
+			<div class="span12 nopad group">
+				<div class="span4"><?php get_sidebar( 'footer' ); ?></div>
+				<div class="span4"><?php get_sidebar( 'footer' ); ?></div>
+				<div class="span4"><?php get_sidebar( 'footer' ); ?></div>
+			</div>
 
-			<footer id="main-footer">
-				<?php get_sidebar( 'footer' ); ?>
-
-
-		<?php
-			if ( has_nav_menu( 'footer-menu' ) ) : ?>
-
-				<div id="et-footer-nav">
-					<div class="container">
+			<div id="footer-bottom">
+				<div class="container clearfix">
 						<?php
-							wp_nav_menu( array(
-								'theme_location' => 'footer-menu',
-								'depth'          => '1',
-								'menu_class'     => 'bottom-nav',
-								'container'      => '',
-								'fallback_cb'    => '',
-							) );
+							if ( false !== et_get_option( 'show_footer_social_icons', true ) ) {
+								get_template_part( 'includes/social_icons', 'footer' );
+							}
+
+							echo et_get_footer_credits();
 						?>
-					</div>
-				</div> <!-- #et-footer-nav -->
+				</div>	<!-- .container -->
+			</div>
+			<?php //get_sidebar( 'footer' ); ?>
 
-			<?php endif; ?>
 
-				<div id="footer-bottom">
-					<div class="container clearfix">
-				<?php
-					if ( false !== et_get_option( 'show_footer_social_icons', true ) ) {
-						get_template_part( 'includes/social_icons', 'footer' );
-					}
+			<?php
+				//if ( has_nav_menu( 'footer-menu' ) ) : ?>
 
-					echo et_get_footer_credits();
-				?>
-					</div>	<!-- .container -->
-				</div>
+				<!-- <div id="et-footer-nav">
+					<div class="container"> -->
+						<?php
+							// wp_nav_menu( array(
+							// 	'theme_location' => 'footer-menu',
+							// 	'depth'          => '1',
+							// 	'menu_class'     => 'bottom-nav',
+							// 	'container'      => '',
+							// 	'fallback_cb'    => '',
+							// ) );
+						?>
+					<!-- </div>
+				</div> --> <!-- #et-footer-nav -->
+
+			<?php //endif; ?>
+
+				<!-- <div id="footer-bottom">
+					<div class="container clearfix"> -->
+						<?php
+							// if ( false !== et_get_option( 'show_footer_social_icons', true ) ) {
+							// 	get_template_part( 'includes/social_icons', 'footer' );
+							// }
+
+							// echo et_get_footer_credits();
+						?>
+					<!-- </div> -->	<!-- .container -->
+				<!-- </div> -->
 			</footer> <!-- #main-footer -->
 		</div> <!-- #et-main-area -->
 
-<?php endif; // ! is_page_template( 'page-template-blank.php' ) ?>
+		<?php endif; // ! is_page_template( 'page-template-blank.php' ) ?>
 
 	</div> <!-- #page-container -->
 
